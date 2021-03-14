cd9120c6affa1fe91229f76e31796aa252254cf6
Rob Dixon <rob@planettelex.net>
2021-03-14 13:45:47 -0600

    #19 Added services.sql

---
+++ Server/SongtrackerPro.Data/Seed/services.sql
@@ -0,0 +1,16 @@
+﻿BEGIN TRANSACTION;
+GO
+
+INSERT INTO [services] ([name]) VALUES ('Interactive Streaming');
+INSERT INTO [services] ([name]) VALUES ('Non-Interactive Streaming');
+INSERT INTO [services] ([name]) VALUES ('Live Streaming');
+INSERT INTO [services] ([name]) VALUES ('Video Hosting');
+INSERT INTO [services] ([name]) VALUES ('Video Clips');
+INSERT INTO [services] ([name]) VALUES ('Social Media');
+INSERT INTO [services] ([name]) VALUES ('Digital Sales');
+INSERT INTO [services] ([name]) VALUES ('Physical Sales');
+INSERT INTO [services] ([name]) VALUES ('Payment');
+GO
+
+COMMIT;
+GO
